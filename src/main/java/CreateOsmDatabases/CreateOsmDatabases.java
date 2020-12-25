a621ea447fd037e54fb39d9573a386f29fcf2f2c
gpesquero <gpesquero@yahoo.es>
2020-12-25 18:54:55 +0100

    Added use of Log class

--- src/main/java/CreateOsmDatabases/CreateOsmDatabases.java
+++ src/main/java/CreateOsmDatabases/CreateOsmDatabases.java
@@ -3,79 +3,82 @@ package CreateOsmDatabases;
 import java.io.File;
 import java.io.FilenameFilter;
 
+import org.utilslibrary.Log;
 import org.utilslibrary.OsmDatabase;
 import org.utilslibrary.OsmPbfFile;
 
 public class CreateOsmDatabases {
 	
+	static final String APP_NAME = "CreateOsmDabases";
+	
 	static final String PBF_FILE_EXT =".osm.pbf";
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Starting...");
+		Log.info("Starting " + APP_NAME + "...");
 		
-		if (args.length<1) {
+		if (args.length < 1) {
 			
-			System.out.println("Missing argument <BaseDir>. Quitting...");
+			Log.error("Missing argument <BaseDir>. Quitting...");
 			
 			return;
 		}
 		
-		String mapsDirName=args[0];
+		String mapsDirName = args[0];
 		
-		System.out.println("Maps DirName=<"+mapsDirName+">");
+		Log.info("Maps DirName=<" + mapsDirName + ">");
 		
-		File mapsDir=new File(mapsDirName);
+		File mapsDir = new File(mapsDirName);
 		
 		if (!mapsDir.isDirectory()) {
 			
-			System.out.println("MapsDir <"+mapsDirName+"> is not a directory. Quitting...");
+			Log.error("MapsDir <" + mapsDirName + "> is not a directory. Quitting...");
 			
 			return;
 		}
 		
-		FilenameFilter filter=new FilenameFilter() {
+		FilenameFilter filter = new FilenameFilter() {
 			
 			public boolean accept(File directory, String fileName) {
 			        return fileName.endsWith(PBF_FILE_EXT);
 			    }
 			};
 		
-		File[] pbfFiles=mapsDir.listFiles(filter);
+		File[] pbfFiles = mapsDir.listFiles(filter);
 		
-		System.out.println("Found "+pbfFiles.length+" PBF files (*"+PBF_FILE_EXT+")");
+		Log.info("Found " + pbfFiles.length + " PBF files (*" + PBF_FILE_EXT + ")");
 		
-		for(int file=0; file<pbfFiles.length; file++) {
+		for(int file = 0; file < pbfFiles.length; file++) {
 			
 			OsmPbfFile pbf = new OsmPbfFile();
 		
 			if (!pbf.openFile(pbfFiles[file].getAbsolutePath())) {
 			
-				System.out.println("Error opening PBF File <"+pbfFiles[file].getAbsolutePath()+">. Go to next one...");
+				Log.error("Error opening PBF File <" + pbfFiles[file].getAbsolutePath() + ">. Go to next one...");
 			
 				continue;
 			}
 			
-			String mapName=pbfFiles[file].getName().replaceFirst(PBF_FILE_EXT, "");
+			String mapName = pbfFiles[file].getName().replaceFirst(PBF_FILE_EXT, "");
 			
-			System.out.println("Map Name: <"+mapName+">");
+			Log.info("Map Name: <" + mapName + ">");
 			
-			File dbFile=new File(mapsDir, mapName+".db");
+			File dbFile = new File(mapsDir, mapName + ".db");
 			
-			System.out.println("DB Filename: <"+dbFile.getAbsolutePath()+">");
+			Log.info("DB Filename: <" + dbFile.getAbsolutePath() + ">");
 			
 			if (dbFile.exists()) {
 				
-				System.out.println("DB <"+dbFile.getAbsolutePath()+"> already exists. Go to next one...");
+				Log.info("DB <" + dbFile.getAbsolutePath() + "> already exists. Go to next one...");
 				
 				continue;
 			}
 			
-			OsmDatabase db=new OsmDatabase();
+			OsmDatabase db = new OsmDatabase();
 			
 			if (!db.createDatabase(dbFile.getAbsolutePath())) {
 				
-				System.out.println("Error opening OSM Database. Quitting...");
+				Log.error("Error opening OSM Database. Quitting...");
 				
 				return;
 			}
@@ -84,12 +87,16 @@ public class CreateOsmDatabases {
 			
 			if (!pbf.process(db)) {
 				
-				System.out.println("Error processing PBF File <"+pbfFiles[file].getAbsolutePath()+">. Quitting...");
+				Log.error("Error processing PBF File <"+pbfFiles[file].getAbsolutePath()+">. Quitting...");
 				
 				return;
-			}		
+			}
+			
+			db.saveDatabaseInfo(pbf);
+			
+			db.closeDatabase();
 		}
 		
-		System.out.println("Finished...");	
+		Log.info("Finished " + APP_NAME + "...");	
 	}
 }
